0f9639d568ddb08d010e2820eb259513381d62e3
Shane Ewert <shane.d.ewert@gmail.com>
2016-07-08 14:44:14 -0500

    Bumped Anroid version

--- scripts/app-version.js
+++ scripts/app-version.js
@@ -1,7 +1,7 @@
 module.exports = function(robot) {
 
     var IOS_VERSION = '4.3.23';
-    var ANDROID_VERSION = '4.5.773';
+    var ANDROID_VERSION = '4.5.774';
 
     function androidVersion(reply) {
         reply.send(ANDROID_VERSION);
