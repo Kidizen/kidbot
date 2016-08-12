885d51cf3c44c18ae56fdfa7d1488bf0be6a1d56
Shane Ewert <shane.d.ewert@gmail.com>
2016-08-12 16:16:54 -0500

    Variables instead of hardcoding

--- scripts/app-version.js
+++ scripts/app-version.js
@@ -1,15 +1,24 @@
 module.exports = function(robot) {
 
-    var IOS_VERSION = '4.5.3';
-    var ANDROID_VERSION = '4.5.783';
-
     function androidVersion(reply) {
-        reply.send(ANDROID_VERSION);
+        reply.send(robot.brain.get('androidVersion'));
     }
 
     function iOSVersion(reply) {
-        reply.send(IOS_VERSION);
+        reply.send(robot.brain.get('iosVersion'));
     }
+
+    robot.respond(/set android version to (.*)/i, function(reply) {
+        var version = reply.match[1];
+        robot.brain.set('androidVersion', version);
+        reply.send('Set Android version to ' + version);
+    });
+
+    robot.respond(/set ios version to (.*)/i, function(reply) {
+        var version = reply.match[1];
+        robot.brain.set('iosVersion', version);
+        reply.send('Set iOS version to ' + version);
+    });
 
     robot.hear(/.*android version.*/i, function(reply) {
         androidVersion(reply);
