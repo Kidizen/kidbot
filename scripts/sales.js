664774a1a2ea153107fcd3700a16819899ee5769
Shane Ewert <shane.d.ewert@gmail.com>
2017-08-16 08:20:07 -0500

    Added web

--- scripts/sales.js
+++ scripts/sales.js
@@ -14,8 +14,8 @@
 //   hubot sales detail - show me more info about the money
 //   hubot when is whiskey - when will it be whiskey time?
 //   hubot set whiskey bar to N - raise/lower the bar to N dollars
-//   hubot [total|ios|android|order|label] [record|milestone] - 
-//   hubot set[total|ios|android|order|label] [record|milestone] to N
+//   hubot [total|ios|android|web|order|label] [record|milestone] -
+//   hubot set[total|ios|android|web|order|label] [record|milestone] to N
 
 module.exports = function(robot) {
 
@@ -32,7 +32,7 @@ module.exports = function(robot) {
       idleTimeoutMillis: 1000 * 60 * 5
     });
 
-    var QUERY = "select distinct d.date as date, coalesce(pbs.order_amount,0) + coalesce(kl.label_amount,0) as total, pbs.order_amount as order, coalesce(kl.label_amount,0) as label, pbs.ios_amount as ios, pbs.android_amount as android, pbs.kb_amount as kidbucks, pbs.ss_amount as selfship, pbs.ppl_amount as ppl, round(pbs.ios_amount::decimal/pbs.order_amount, 4) as ios_percent, round(pbs.android_amount::decimal/pbs.order_amount, 4) as android_percent, round(pbs.kb_amount::decimal/pbs.order_amount, 4) as kidbucks_percent from(select date_trunc('day', dd)::timestamp as date from generate_series(now() AT TIME ZONE 'CDT' - interval '7 days', CURRENT_TIMESTAMP AT TIME ZONE 'CDT', '1 day'::interval) dd) as d left join (select tmp.order_date, sum(tmp.amount) as order_amount, sum(case when tmp.created_through = 'ios' then tmp.amount else 0 end) as ios_amount, sum(case when tmp.created_through = 'android' then tmp.amount else 0 end) as android_amount, sum(case when tmp.seller_fee_strategy = 'Percentage' then tmp.amount else 0 end) as kb_amount, sum(case when tmp.seller_fee_strategy = 'PaypalSelfship' then tmp.amount else 0 end) as ss_amount, sum(case when tmp.seller_fee_strategy = 'PaypalPpl' then tmp.amount else 0 end) as ppl_amount from (select date_trunc('day',(o.purchase_date::TIMESTAMP WITH TIME ZONE) AT TIME ZONE 'CDT') as order_date, o.id, o.user_id, o.seller_id, case when o.created_through is null then case when u.created_through = 'android' then u.created_through else 'ios' end else o.created_through end as created_through, o.fee_strategy_info->>'strategy_name' as seller_fee_strategy, round(pay.amount_cents/100.0,2) as amount from orders o inner join (select p.order_id, sum(p.amount_cents) as amount_cents from payments as p where p.aasm_state = 'successful' group by p.order_id) pay on pay.order_id = o.id left join users u on o.user_id = u.id where (o.purchase_date::TIMESTAMP WITH TIME ZONE) AT TIME ZONE 'CDT' >= NOW() - interval '14 days' and o.aasm_state = 'completed' and o.user_id <> 0 order by (o.purchase_date::TIMESTAMP WITH TIME ZONE) AT TIME ZONE 'CDT') tmp group by tmp.order_date) pbs on pbs.order_date = d.date left join (select date_trunc('day',(kl.created_at::TIMESTAMP WITH TIME ZONE) AT TIME ZONE 'CDT') as date, round(sum(kl.amount_cents)/100.0,2) as label_amount from kid_labels kl left join shipments sh on sh.id = kl.shipment_id where sh.aasm_state not in ('canceled','failed') and kl.payment_method_type is NOT NULL group by date_trunc('day',(kl.created_at::TIMESTAMP WITH TIME ZONE) AT TIME ZONE 'CDT')) kl on kl.date = d.date order by 1 desc";
+    var QUERY = "select distinct d.date as date, coalesce(pbs.order_amount,0) + coalesce(kl.label_amount,0) as total, pbs.order_amount as order, coalesce(kl.label_amount,0) as label, pbs.ios_amount as ios, pbs.android_amount as android, pbs.web_amount as web, round(pbs.ios_amount::decimal/pbs.order_amount, 4) as ios_percent, round(pbs.android_amount::decimal/pbs.order_amount, 4) as android_percent, round(pbs.web_amount::decimal/pbs.order_amount, 4) as web_percent from(select date_trunc('day', dd)::timestamp as date from generate_series(now() AT TIME ZONE 'CDT' - interval '7 days', CURRENT_TIMESTAMP AT TIME ZONE 'CDT', '1 day'::interval) dd) as d left join (select tmp.order_date, sum(tmp.amount) as order_amount, sum(case when tmp.created_through = 'ios' then tmp.amount else 0 end) as ios_amount, sum(case when tmp.created_through = 'android' then tmp.amount else 0 end) as android_amount, sum(case when tmp.created_through = 'web' then tmp.amount else 0 end) as web_amount from (select date_trunc('day',(o.purchase_date::TIMESTAMP WITH TIME ZONE) AT TIME ZONE 'CDT') as order_date, o.id, o.user_id, o.seller_id, case when o.created_through is null then case when u.created_through = 'android' then u.created_through else 'ios' end else o.created_through end as created_through, o.fee_strategy_info->>'strategy_name' as seller_fee_strategy, round(pay.amount_cents/100.0,2) as amount from orders o inner join (select p.order_id, sum(p.amount_cents) as amount_cents from payments as p where p.aasm_state = 'successful' group by p.order_id) pay on pay.order_id = o.id left join users u on o.user_id = u.id where (o.purchase_date::TIMESTAMP WITH TIME ZONE) AT TIME ZONE 'CDT' >= NOW() - interval '14 days' and o.aasm_state = 'completed' and o.user_id <> 0 order by (o.purchase_date::TIMESTAMP WITH TIME ZONE) AT TIME ZONE 'CDT') tmp group by tmp.order_date) pbs on pbs.order_date = d.date left join (select date_trunc('day',(kl.created_at::TIMESTAMP WITH TIME ZONE) AT TIME ZONE 'CDT') as date, round(sum(kl.amount_cents)/100.0,2) as label_amount from kid_labels kl left join shipments sh on sh.id = kl.shipment_id where sh.aasm_state not in ('canceled','failed') and kl.payment_method_type is NOT NULL group by date_trunc('day',(kl.created_at::TIMESTAMP WITH TIME ZONE) AT TIME ZONE 'CDT')) kl on kl.date = d.date order by 1 desc";
 
     Number.prototype.format = function(n, x) {
         var re = '\\d(?=(\\d{' + (x || 3) + '})+' + (n > 0 ? '\\.' : '$') + ')';
@@ -77,8 +77,10 @@ module.exports = function(robot) {
                         label: toMoney(row.label),
                         ios: toMoney(row.ios),
                         android: toMoney(row.android),
+                        web: toMoney(row.web),
                         iosPercent: toPercent(row.ios_percent),
-                        androidPercent: toPercent(row.android_percent)
+                        androidPercent: toPercent(row.android_percent),
+                        webPercent: toPercent(row.web_percent)
                     });
                 }
             });
@@ -115,6 +117,10 @@ module.exports = function(robot) {
             reply.send(':android: New record! ' + res.android);
             robot.brain.set('androidRecord', res.android);
         }
+        if (toNum(res.web) > toNum(robot.brain.get('webRecord'))) {
+            reply.send(':desktop_computer: New record! ' + res.web);
+            robot.brain.set('webRecord', res.web);
+        }
         if (toNum(res.order) > toNum(robot.brain.get('orderRecord'))) {
             reply.send(':dress: New record! ' + res.order);
             robot.brain.set('orderRecord', res.order);
@@ -125,12 +131,12 @@ module.exports = function(robot) {
         }
     }
 
-    robot.respond(/.*(total|ios|android|order|label) (?:record|milestone).*/i, function(reply) {
+    robot.respond(/.*(total|ios|android|web|order|label) (?:record|milestone).*/i, function(reply) {
         var record = reply.match[1].toLowerCase();
         reply.send(robot.brain.get(record + 'Record'));
     });
 
-    robot.respond(/.*set (total|ios|android|order|label) (?:record|milestone) to (.*)/i, function(reply) {
+    robot.respond(/.*set (total|ios|android|web|order|label) (?:record|milestone) to (.*)/i, function(reply) {
         var record = reply.match[1];
         var value = reply.match[2];
         robot.brain.set(record + 'Record', value);
@@ -156,7 +162,7 @@ module.exports = function(robot) {
         bar = parseInt(bar.trim().replace('$', '').replace(',', ''));
         robot.brain.set('whiskeyBar', bar);
         reply.send(whiskey + ' bar set to ' + bar);
-    });    
+    });
 
     robot.hear(/kidbot (sales|:money_mouth_face:|🤑).*/i, function(reply) {
         reply.send('One sec...');
@@ -169,7 +175,7 @@ module.exports = function(robot) {
     robot.hear(/kidbot (sales|:money_mouth_face:|🤑) detail.*/i, function(reply) {
         reply.send('One sec...');
         getSalesInfo(reply, function(res) {
-            reply.send(':moneybag: ' + res.total + '\n:dress: ' + res.order + '\n :label: ' + res.label + '\n :ios: ' + res.iosPercent + '\n :android: ' + res.androidPercent);
+            reply.send(':moneybag: ' + res.total + '\n:dress: ' + res.order + '\n :label: ' + res.label + '\n :ios: ' + res.iosPercent + '\n :android: ' + res.androidPercent + '\n :desktop_computer: ' + res.webPercent);
             checkForRecord(reply, res);
         });
     });
