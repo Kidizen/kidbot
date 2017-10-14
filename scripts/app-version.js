13e9656668702aa8343ea2daccc19411c7fec9fe
Shane Ewert <shane.d.ewert@gmail.com>
2017-10-14 08:06:19 -0500

    Added dates to released for iOS and Android, take 1

--- scripts/app-version.js
+++ scripts/app-version.js
@@ -1,35 +1,44 @@
+let dateFormat = require('dateformat');
+let time = require('time-ago')();
+
 module.exports = function(robot) {
 
     function androidVersion(reply) {
-        reply.send(robot.brain.get('androidVersion'));
+        let version = robot.brain.get('androidVersion');
+        let lastReleaseDate = new Date(robot.brain.get('androidReleaseDate') || Date.now());
+        let lastRelease = time.ago(lastReleaseDate) + ' on ' + dateFormat(lastReleaseDate, 'dddd, mmmm dS');
+        reply.send(version + ' (released ' + lastRelease + ')');
     }
 
     function iOSVersion(reply) {
-        reply.send(robot.brain.get('iosVersion'));
+        let version = robot.brain.get('iOSVersion');
+        let lastReleaseDate = new Date(robot.brain.get('iOSReleaseDate') || Date.now());
+        let lastRelease = time.ago(lastReleaseDate) + ' on ' + dateFormat(lastReleaseDate, 'dddd, mmmm dS');
+        reply.send(version + ' (released ' + lastRelease + ')');
     }
 
     robot.respond(/set android version to (.*)/i, function(reply) {
         var version = reply.match[1];
         robot.brain.set('androidVersion', version);
-        reply.send('Set Android version to ' + version);
+        robot.brain.set('androidReleaseDate', Date.now());
+        reply.send('Version now set to ' + version);
     });
 
     robot.respond(/set ios version to (.*)/i, function(reply) {
         var version = reply.match[1];
         robot.brain.set('iosVersion', version);
-        reply.send('Set iOS version to ' + version);
+        robot.brain.set('iOSReleaseDate', Date.now());
+        reply.send('Version now set to ' + version);
     });
 
-    robot.hear(/.*android version.*/i, function(reply) {
-        androidVersion(reply);
+    robot.hear(/.*android (version|release).*/i, function(reply) {
+        if (!/set.*version/.test(reply.match[0])) {
+            androidVersion(reply);
+        }
     });
-    robot.hear(/.*ios version.*/i, function(reply) {
-        iOSVersion(reply);
-    });
-    robot.hear(/.*android release.*/i, function(reply) {
-        androidVersion(reply);
-    });
-    robot.hear(/.*ios release.*/i, function(reply) {
-        iOSVersion(reply);
+    robot.hear(/.*ios (version|release).*/i, function(reply) {
+        if (!/set.*version/.test(reply.match[0])) {
+            iOSVersion(reply);
+        }
     });
 }
