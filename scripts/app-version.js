42e2dcc28e8f2171110098b23f8221e663bac5f8
Shane Ewert <shane.d.ewert@gmail.com>
2017-10-15 08:46:20 -0500

    Version tag reminder

--- scripts/app-version.js
+++ scripts/app-version.js
@@ -3,6 +3,10 @@ let time = require('time-ago')();
 
 module.exports = function(robot) {
 
+    function getTagInstructions(version) {
+        return '\n\n_Did you remember to tag the release?_\n```\n$ git checkout master\n$ git pull\n$ git tag v' + version + '\n$ git push origin v' + version + '\n```';
+    }
+
     function androidVersion(reply) {
         let version = robot.brain.get('androidVersion');
         let lastReleaseDate = new Date(robot.brain.get('androidReleaseDate') || Date.now());
@@ -21,14 +25,14 @@ module.exports = function(robot) {
         var version = reply.match[1];
         robot.brain.set('androidVersion', version);
         robot.brain.set('androidReleaseDate', Date.now());
-        reply.send('Version now set to ' + version);
+        reply.send('Version now set to ' + version + getTagInstructions(version));
     });
 
     robot.respond(/set ios version to (.*)/i, function(reply) {
         var version = reply.match[1];
         robot.brain.set('iosVersion', version);
         robot.brain.set('iosReleaseDate', Date.now());
-        reply.send('Version now set to ' + version);
+        reply.send('Version now set to ' + version + getTagInstructions(version));
     });
 
     robot.hear(/.*android (version|release).*/i, function(reply) {
