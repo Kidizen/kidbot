11203b20d831603e876fd36126fb3f7c9938d285
Shane Ewert <shane.d.ewert@gmail.com>
2018-08-14 21:21:59 -0500

    Revert again

--- scripts/sales.js
+++ scripts/sales.js
@@ -37,6 +37,7 @@ module.exports = function(robot) {
     let HOUR_OFFSET = TIMEZONE == 'CDT' ? 5 : 6;
     let MILLESECOND_OFFSET = (HOUR_OFFSET*60*60*1000);
     let ONE_DAY_MILLIS = 24*60*60*1000;
+    let QUERY = "select distinct d.date as date, coalesce(pbs.order_amount,0) + coalesce(kl.label_amount,0) as total, pbs.order_amount as order, coalesce(kl.label_amount,0) as label, pbs.ios_amount as ios, pbs.android_amount as android, pbs.web_amount as web, round(pbs.ios_amount::decimal/pbs.order_amount, 4) as ios_percent, round(pbs.android_amount::decimal/pbs.order_amount, 4) as android_percent, round(pbs.web_amount::decimal/pbs.order_amount, 4) as web_percent from(select date_trunc('day', dd)::timestamp as date from generate_series(now() AT TIME ZONE 'CDT' - interval '7 days', CURRENT_TIMESTAMP AT TIME ZONE 'CDT', '1 day'::interval) dd) as d left join (select tmp.order_date, sum(tmp.amount) as order_amount, sum(case when tmp.created_through = 'ios' then tmp.amount else 0 end) as ios_amount, sum(case when tmp.created_through = 'android' then tmp.amount else 0 end) as android_amount, sum(case when tmp.created_through = 'web' then tmp.amount else 0 end) as web_amount from (select date_trunc('day',(o.purchase_date::TIMESTAMP WITH TIME ZONE) AT TIME ZONE 'CDT') as order_date, o.id, o.user_id, o.seller_id, case when o.created_through is null then case when u.created_through = 'android' then u.created_through else 'ios' end else o.created_through end as created_through, o.fee_strategy_info->>'strategy_name' as seller_fee_strategy, round(pay.amount_cents/100.0,2) as amount from orders o inner join (select p.order_id, sum(p.amount_cents) as amount_cents from payments as p where p.aasm_state = 'successful' group by p.order_id) pay on pay.order_id = o.id left join users u on o.user_id = u.id where (o.purchase_date::TIMESTAMP WITH TIME ZONE) AT TIME ZONE 'CDT' >= NOW() - interval '14 days' and o.aasm_state = 'completed' and o.user_id <> 0 order by (o.purchase_date::TIMESTAMP WITH TIME ZONE) AT TIME ZONE 'CDT') tmp group by tmp.order_date) pbs on pbs.order_date = d.date left join (select date_trunc('day',(kl.created_at::TIMESTAMP WITH TIME ZONE) AT TIME ZONE 'CDT') as date, round(sum(kl.amount_cents)/100.0,2) as label_amount from kid_labels kl left join shipments sh on sh.id = kl.shipment_id where sh.aasm_state not in ('canceled','failed') and kl.payment_method_type is NOT NULL group by date_trunc('day',(kl.created_at::TIMESTAMP WITH TIME ZONE) AT TIME ZONE 'CDT')) kl on kl.date = d.date order by 1 desc";
 
     Number.prototype.format = function(n, x) {
         var re = '\\d(?=(\\d{' + (x || 3) + '})+' + (n > 0 ? '\\.' : '$') + ')';
@@ -49,35 +50,39 @@ module.exports = function(robot) {
         return new Date(now.getTime() + MILLESECOND_OFFSET);
     }
 
-    function getQuery() {
+    function getLabelsQuery() {
         return "SELECT \
-                round((tmp.gross_sales_cents - tmp.refunded_sales_cents + tmp.gross_labels - tmp.refunded_labels - tmp.refunded_label_fees) / 100.0, 2) AS total, \
-                round((tmp.gross_sales_cents - tmp.refunded_sales_cents) / 100.0, 2) AS order, \
-                round((tmp.gross_labels - tmp.refunded_labels - tmp.refunded_label_fees) / 100.0, 2) AS label, \
-                round((tmp.gross_ios_cents - tmp.refunded_ios_cents) / 100.0, 2) AS ios, \
-                round((tmp.gross_android_cents - tmp.refunded_android_cents) / 100.0, 2) AS android, \
-                round((tmp.gross_web_cents - tmp.refunded_web_cents) / 100.0, 2) AS web, \
-                round((tmp.gross_ios_cents - tmp.refunded_ios_cents)/(tmp.gross_sales_cents - tmp.refunded_sales_cents * 1.0), 2) AS ios_percent, \
-                round((tmp.gross_android_cents - tmp.refunded_android_cents)/(tmp.gross_sales_cents - tmp.refunded_sales_cents * 1.0), 2) AS android_percent, \
-                round((tmp.gross_web_cents - tmp.refunded_web_cents)/(tmp.gross_sales_cents - tmp.refunded_sales_cents * 1.0), 2) AS web_percent \
-            FROM (SELECT \
-                    SUM(p.amount_cents) AS gross_sales_cents, \
-                    SUM(p.refunded_amount_cents) AS refunded_sales_cents, \
-                    SUM(CASE WHEN o.created_through = 'ios' THEN p.amount_cents ELSE 0 END) AS gross_ios_cents, \
-                    SUM(CASE WHEN o.created_through = 'ios' THEN p.refunded_amount_cents ELSE 0 END) AS refunded_ios_cents, \
-                    SUM(CASE WHEN o.created_through = 'android' THEN p.amount_cents ELSE 0 END) AS gross_android_cents, \
-                    SUM(CASE WHEN o.created_through = 'android' THEN p.refunded_amount_cents ELSE 0 END) AS refunded_android_cents, \
-                    SUM(CASE WHEN o.created_through = 'web' THEN p.amount_cents ELSE 0 END) as gross_web_cents, \
-                    SUM(CASE WHEN o.created_through = 'web' THEN p.refunded_amount_cents ELSE 0 END) AS refunded_web_cents, \
-                    SUM(COALESCE(l.amount_cents,0)) AS gross_labels, \
-                    SUM(COALESCE(l.refunded_amount_cents,0)) AS refunded_labels, \
-                    SUM(COALESCE(l.refunded_marketplace_fee_cents ,0)) AS refunded_label_fees \
-                FROM payments p \
-                INNER JOIN orders o ON o.id = p.order_id \
-                LEFT OUTER JOIN shipments s ON o.id = s.order_id \
-                LEFT OUTER JOIN kid_labels l ON s.id = l.shipment_id \
-                WHERE o.aasm_state = 'completed' \
-                AND o.created_at >= '" + dateFormat(getLocalTime(), "yyyy-mm-dd'T'HH:MM:ss")  + "') AS tmp";
+                    round((tmp.gross_labels - tmp.refunded_labels - tmp.refunded_label_fees) / 100.0, 2) AS label \
+                FROM (SELECT  \
+                        SUM(COALESCE(l.amount_cents,0)) AS gross_labels, \
+                        SUM(COALESCE(l.refunded_amount_cents,0)) AS refunded_labels, \
+                        SUM(COALESCE(l.refunded_marketplace_fee_cents ,0)) AS refunded_label_fees \
+                    FROM kid_labels l \
+                    WHERE l.created_at >= '" + getLocalTime() + "') AS tmp";
+    }
+
+    function getOrdersQuery() {
+        return "SELECT \
+                    round((tmp.gross_sales_cents - tmp.refunded_sales_cents) / 100.0, 2) AS order, \
+                    round((tmp.gross_ios_cents - tmp.refunded_ios_cents) / 100.0, 2) AS ios, \
+                    round((tmp.gross_android_cents - tmp.refunded_android_cents) / 100.0, 2) AS android, \
+                    round((tmp.gross_web_cents - tmp.refunded_web_cents) / 100.0, 2) AS web, \
+                    round((tmp.gross_ios_cents - tmp.refunded_ios_cents)/(tmp.gross_sales_cents - tmp.refunded_sales_cents * 1.0), 2) AS ios_percent, \
+                    round((tmp.gross_android_cents - tmp.refunded_android_cents)/(tmp.gross_sales_cents - tmp.refunded_sales_cents * 1.0), 2) AS android_percent, \
+                    round((tmp.gross_web_cents - tmp.refunded_web_cents)/(tmp.gross_sales_cents - tmp.refunded_sales_cents * 1.0), 2) AS web_percent \
+                FROM (SELECT  \
+                        SUM(p.amount_cents) AS gross_sales_cents, \
+                        SUM(p.refunded_amount_cents) AS refunded_sales_cents, \
+                        SUM(CASE WHEN o.created_through = 'ios' THEN p.amount_cents ELSE 0 END) AS gross_ios_cents, \
+                        SUM(CASE WHEN o.created_through = 'ios' THEN p.refunded_amount_cents ELSE 0 END) AS refunded_ios_cents, \
+                        SUM(CASE WHEN o.created_through = 'android' THEN p.amount_cents ELSE 0 END) AS gross_android_cents, \
+                        SUM(CASE WHEN o.created_through = 'android' THEN p.refunded_amount_cents ELSE 0 END) AS refunded_android_cents, \
+                        SUM(CASE WHEN o.created_through = 'web' THEN p.amount_cents ELSE 0 END) as gross_web_cents, \
+                        SUM(CASE WHEN o.created_through = 'web' THEN p.refunded_amount_cents ELSE 0 END) AS refunded_web_cents \
+                    FROM payments p \
+                    INNER JOIN orders o ON o.id = p.order_id \
+                    WHERE o.aasm_state = 'completed' \
+                    AND o.created_at >= '" + getLocalTime() + "') AS tmp";
     }
 
     function toMoney(str) {
@@ -96,6 +101,52 @@ module.exports = function(robot) {
         }
     }
 
+    function getSalesInfo2(reply, onSuccess) {
+        pool.connect(function(err, client, done) {
+
+            if (err) {
+                reply.send('Poop. I fail: ' + err);
+                return;
+            }
+
+            // get orders data first
+            client.query(getOrdersQuery(), function(err, result) {
+
+                if (err) {
+                    done();
+                    reply.send('Poop. I fail: ' + err);
+                } else {
+
+                    // success! now get labels data
+                    var ordersRow = result.rows[0];
+                    client.query(getLabelsQuery(), function(err, result) {
+
+                        done();
+
+                        if (err) {
+                            reply.send('Poop. I fail: ' + err);
+                        } else {
+                            var labelsRow = result.rows[0];
+                            var total = parseFloat(ordersRow.order).format(2) + parseFloat(labelsRow.label).format(2);
+
+                            onSuccess({
+                                total: toMoney(total),
+                                order: toMoney(ordersRow.order),
+                                label: toMoney(labelsRow.label),
+                                ios: toMoney(ordersRow.ios),
+                                android: toMoney(ordersRow.android),
+                                web: toMoney(ordersRow.web),
+                                iosPercent: toPercent(ordersRow.ios_percent),
+                                androidPercent: toPercent(ordersRow.android_percent),
+                                webPercent: toPercent(ordersRow.web_percent)
+                            });
+                        }
+                    });
+                }
+            });
+        });
+    }
+
     function getSalesInfo(reply, onSuccess) {
         pool.connect(function(err, client, done) {
 
@@ -104,7 +155,7 @@ module.exports = function(robot) {
                 return;
             }
 
-            client.query(getQuery(), function(err, result) {
+            client.query(QUERY, function(err, result) {
 
                 done();
 
